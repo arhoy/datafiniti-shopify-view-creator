9655077d6aa65b5d7baef2bd4c1f89bebf0f5b7b
arhoy <arhoy@ualberta.ca>
2019-12-04 02:20:24 -0700

    updated config file, delete default.json

--- config/db.js
+++ config/db.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
 
-const config = require('config');
-
-// get databse connection string
-const db = config.get('mongoURI');
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
-    console.log('Mongo db connected'.underline.brightGreen.bold);
-  } catch (error) {
-    console.error('There was an error', error.message);
-    process.exit(1);
-  }
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  console.log(
+    `MongoDB Connected: ${conn.connection.host}`.green.underline.bold,
+  );
 };
 
 module.exports = connectDB;
