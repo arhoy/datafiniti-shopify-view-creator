b15bba75bf67283bf4f5801d0f5e249606ea64dd
arhoy <you@example.com>
2019-12-15 18:40:19 -0700

    fixed pagination issue

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,26 +1,31 @@
-// Advanced Results for sorting and Pagination
-
 const advancedResults = (model, populate) => async (req, res, next) => {
   let query;
 
+  // Copy req.query
   const reqQuery = { ...req.query };
 
+  // Fields to exclude
   const removeFields = ['select', 'sort', 'page', 'limit'];
 
+  // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
 
+  // Create query string
   let queryStr = JSON.stringify(reqQuery);
 
+  // Create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-  // query
+  // Finding resource
   query = model.find(JSON.parse(queryStr));
 
+  // Select Fields
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
 
+  // Sort
   if (req.query.sort) {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
@@ -30,7 +35,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
 
   // Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.page, 10) || 25;
+  const limit = parseInt(req.query.limit, 10) || 25;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await model.countDocuments();
@@ -41,7 +46,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
     query = query.populate(populate);
   }
 
-  // Execute query
+  // Executing query
   const results = await query;
 
   // Pagination result
@@ -63,8 +68,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
 
   res.advancedResults = {
     success: true,
-    items: results.length,
-    totalItems: total,
+    count: results.length,
     pagination,
     data: results,
   };
